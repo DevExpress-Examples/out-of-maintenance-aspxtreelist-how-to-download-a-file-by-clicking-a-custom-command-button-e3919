dc2056a2f1adac5b82716780bcbee9991e021411
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# ASPxTreeList - How to download a file by clicking a custom command button
+
+
+<p><strong>See also:</strong></p><p><a href="https://www.devexpress.com/Support/Center/p/E2577">How to load a file on the callback of the ASPxGridView using the ASPxWebControl.RedirectOnCallback method</a><u><br />
+</u><a href="https://www.devexpress.com/Support/Center/p/E4940">ASPxGridViewExporter - How to show content document in a new tab</a><u><br />
+</u></p>
+
+<br/>
+
+
---
+++ VB/WebSite/LoadFile.aspx.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class LoadFile
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim fileName As String = "Images/" & Request.Params(0) & ".jpg"
+		Dim filePath As String = Server.MapPath(fileName)
+
+		Response.AddHeader("Content-Disposition", "attachment; filename=" & Request.Params(0) & ".jpg")
+		Response.ContentType = "image/x-jpg"
+		Response.TransmitFile(filePath)
+		Response.End()
+	End Sub
+End Class
